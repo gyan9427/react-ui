de268ab79e82926335250dad1236a9300fca07cd
gyan <gyan9427@gmail.com>
2021-09-01 10:22:39 +0530

    add responsive typography to Notes.js

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import {Typography} from '@material-ui/core';
+import React from 'react'
+import { Typography, ThemeProvider, createTheme } from '@material-ui/core'
+
+const theme = createTheme()
+
+theme.typography.h1 = {
+  fontSize: '1.2rem',
+  '@media (min-width:600px)': {
+    fontSize: '1.5rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2.4rem',
+  },
+}
 
 export default function Notes() {
   return (
     <div>
-      <Typography
-      variant = "h5"
-      component = "h1"
-      align ="left"
-      gutterBottom
-      >
+      <ThemeProvider theme={theme}>
+        <Typography variant='h1' align='left' gutterBottom>
           Notes page
-      </Typography>
+        </Typography>
+      </ThemeProvider>
     </div>
   )
 }
